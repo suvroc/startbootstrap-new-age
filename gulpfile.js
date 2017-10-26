27d46b83b49623055258161bbf0ec69f965472fc
suvroc <suvroc@o2.pl>
2017-10-26 22:26:34 +0200

    Nexus 5 mockup

--- gulpfile.js
+++ gulpfile.js
@@ -15,139 +15,139 @@ var critical = require('critical');
 
 // Set the banner content
 var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  ''
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+    ' */\n',
+    ''
 ].join('');
 
 var buildPath = 'dist/';
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
-  return gulp.src('scss/new-age.scss')
-    .pipe(sass())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest(path.join(buildPath, 'css')))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    return gulp.src('scss/new-age.scss')
+        .pipe(sass())
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest(path.join(buildPath, 'css')))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 gulp.task('sass-and-min', function() {
-  return gulp.src('scss/new-age.scss')
-    .pipe(sass())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(path.join(buildPath, 'css')))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    return gulp.src('scss/new-age.scss')
+        .pipe(sass())
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(path.join(buildPath, 'css')))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/new-age.css')
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(path.join(buildPath, 'css')))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    return gulp.src(['css/new-age.css'])
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(path.join(buildPath, 'css')))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 // Minify custom JS
 gulp.task('minify-js', function() {
-  return gulp.src('js/new-age.js')
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(path.join(buildPath, 'js')))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    return gulp.src('js/new-age.js')
+        .pipe(uglify())
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(path.join(buildPath, 'js')))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest(path.join(buildPath, 'vendor/bootstrap')))
+    gulp.src([
+            'node_modules/bootstrap/dist/**/*',
+            '!**/npm.js',
+            '!**/bootstrap-theme.*',
+            '!**/*.map'
+        ])
+        .pipe(gulp.dest(path.join(buildPath, 'vendor/bootstrap')))
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest(path.join(buildPath, 'vendor/jquery')))
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest(path.join(buildPath, 'vendor/jquery')))
 
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest(path.join(buildPath, 'vendor/jquery-easing')))
+    gulp.src(['node_modules/jquery.easing/*.js'])
+        .pipe(gulp.dest(path.join(buildPath, 'vendor/jquery-easing')))
 
-  gulp.src(['node_modules/simple-line-icons/*/*'])
-    .pipe(gulp.dest(path.join(buildPath, 'vendor/simple-line-icons')))
+    gulp.src(['node_modules/simple-line-icons/*/*'])
+        .pipe(gulp.dest(path.join(buildPath, 'vendor/simple-line-icons')))
 
-  gulp.src(['index.html'])
-    .pipe(gulp.dest(path.join(buildPath, '')));
+    gulp.src(['index.html'])
+        .pipe(gulp.dest(path.join(buildPath, '')));
 
-  gulp.src(['device-mockups/**/*.css'])
-    .pipe(gulp.dest(path.join(buildPath, 'device-mockups/')));
+    gulp.src(['device-mockups/**/*.css'])
+        .pipe(gulp.dest(path.join(buildPath, 'device-mockups/')));
 
-  gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest(path.join(buildPath, 'vendor/font-awesome')))
+    gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest(path.join(buildPath, 'vendor/font-awesome')))
 });
 
 gulp.task('images', function() {
-  gulp.src(['img/**/*'])
-  .pipe(gulp.dest(path.join(buildPath, 'img/')));
+    gulp.src(['img/**/*'])
+        .pipe(gulp.dest(path.join(buildPath, 'img/')));
 
-  gulp.src(['device-mockups/**/*.png'])
-  .pipe(gulp.dest(path.join(buildPath, 'device-mockups/')));
+    gulp.src(['device-mockups/**/*.png'])
+        .pipe(gulp.dest(path.join(buildPath, 'device-mockups/')));
 });
 
 gulp.task('images-dist', function() {
-  gulp.src(['img/**/*'])
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imageminJpegRecompress({
-        progressive: true,
-        max: 80,
-        min: 70
-    }),
-    imageminPngquant({quality: '75-85'}),
-    imagemin.svgo({plugins: [{removeViewBox: false}]})
-]))
-  .pipe(gulp.dest(path.join(buildPath, 'img/')));
+    gulp.src(['img/**/*'])
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imageminJpegRecompress({
+                progressive: true,
+                max: 80,
+                min: 70
+            }),
+            imageminPngquant({ quality: '75-85' }),
+            imagemin.svgo({ plugins: [{ removeViewBox: false }] })
+        ]))
+        .pipe(gulp.dest(path.join(buildPath, 'img/')));
 
-  gulp.src(['device-mockups/**/*.png'])
-  //.pipe(imagemin())
-  .pipe(gulp.dest(path.join(buildPath, 'device-mockups/')));
+    gulp.src(['device-mockups/**/*.png'])
+        //.pipe(imagemin())
+        .pipe(gulp.dest(path.join(buildPath, 'device-mockups/')));
 });
 
 // Default task
@@ -155,49 +155,47 @@ gulp.task('default', ['sass-and-min', 'minify-css', 'minify-js', 'images-dist', 
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: buildPath 
-    },
-  })
+    browserSync.init({
+        server: {
+            baseDir: buildPath
+        },
+    })
 })
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js', 'images', 'copy'], function() {
-  gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['minify-css']);
-  gulp.watch('js/*.js', ['minify-js']);
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
 });
 
 gulp.task('deploy', ['default', 'critical'], function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages());
 });
 
 gulp.task('optimize-css', function() {
-  return gulp.src('css/*.css')
-      .pipe(autoprefixer())
-      .pipe(uncss({
-          html: ['_site/**/*.html'],
-          ignore: []
-      }))
-      .pipe(minifyCss({keepBreaks: false}))
-      .pipe(gulp.dest(path.join(buildPath,'css/')));
+    return gulp.src('css/*.css')
+        .pipe(autoprefixer())
+        .pipe(uncss({
+            html: ['_site/**/*.html'],
+            ignore: []
+        }))
+        .pipe(minifyCss({ keepBreaks: false }))
+        .pipe(gulp.dest(path.join(buildPath, 'css/')));
 });
 
-gulp.task('critical', ['default'], function (cb) {
-  critical.generate({
-      inline: true,
-      base: 'dist/',
-      src: 'index.html',
-      dest: 'index.html',
-      minify: true,
-      width: 800,
-      height: 600
-  }, cb.bind(cb));
+gulp.task('critical', ['default'], function(cb) {
+    critical.generate({
+        inline: true,
+        base: 'dist/',
+        src: 'index.html',
+        dest: 'index.html',
+        minify: true,
+        width: 800,
+        height: 600
+    }, cb.bind(cb));
 });
-
-
